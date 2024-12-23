46bcebe2f01c1c88726c8ce07069dbf005f77bd1
madelynnn4 <bimovalentino21@gmail.com>
2024-12-23 14:42:13 +0700

    commit

---
+++ src/main/java/com/aka/Doctor.java
@@ -0,0 +1,44 @@
+package com.aka;
+
+import java.util.ArrayList;
+
+
+public class Doctor {
+    private final int id;
+    private String name;
+    protected int workHourStart;
+    protected int workHourEnd;
+    protected int weeklyWorkHour;
+    protected ArrayList<Day> daysUnavailable;
+    protected ArrayList<Day> shift;
+
+    public Doctor(int id, String name, int workHourStart, int workHourEnd, int weeklyWorkHour) {
+        this.id = id;
+        this.name = name;
+        this.workHourStart = workHourStart;
+        this.workHourEnd = workHourEnd;
+        this.weeklyWorkHour = weeklyWorkHour;
+        this.daysUnavailable = new ArrayList<>();
+        this.shift = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void addDaysUnavailable(Day dayUnavailable) {
+        this.daysUnavailable.add(dayUnavailable);
+    }
+
+    public void removeDaysUnavailable(Day dayUnavailable) {
+        this.daysUnavailable.remove(dayUnavailable);
+    }
+}
