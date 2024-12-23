b1669f75042527a8aa2b8d4b4d54092bd7c0d3b8
Nicholas Irawan <nicholas2815@hotmail.com>
2024-12-23 20:14:57 +0700

    iterative testing

--- src/main/java/com/aka/Main.java
+++ src/main/java/com/aka/Main.java
@@ -1,7 +1,18 @@
 package com.aka;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Driver driver = new Driver();
+
+        driver.initialize();
+        driver.setShiftIterative();
+        driver.printDays();
+        System.out.println();
+
+        driver.doctors.get(3).addDaysUnavailable(driver.days[0]);
+        driver.setShiftIterative();
+        driver.printDays();
     }
 }
